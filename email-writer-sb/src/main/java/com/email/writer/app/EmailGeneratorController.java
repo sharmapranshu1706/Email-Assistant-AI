faa44f1422a7943bddb72452c903b0d916425120
Pranshu Sharma <sharma.pranshu1706@gmail.com>
2025-12-20 09:56:52 +0530

    Complete Project....

---
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -0,0 +1,24 @@
+package com.email.writer.app;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/email")
+@RestController
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+
+
+public class EmailGeneratorController {
+    private final EmailGeneratorService emailGeneratorService;
+
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest)
+    // ensure endpoint starts with a leading slash
+    {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
